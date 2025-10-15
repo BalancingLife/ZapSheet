a55141ac064e328cc4b69a73e2f5f51090812483
MJ <alswns8495@naver.com>
2025-10-15 16:25:09 +0900

    ✨ Cell 포커스 기능 구현 (with Zustand)
    - 각 Cell 클릭 시 해당 row,col을 포커스로 설정하도록 Zustand 스토어 추가
    
    - 키보드 방향키(↑↓←→)로 포커스 이동 기능 구현
    
    - 포커스된 Cell에 파란색 테두리 스타일 적용
    
    - Cell 컴포넌트 memo로 감싸 불필요한 리렌더 방지
    
    - tabIndex 및 role 속성으로 접근성과 포커스 가능 상태 개선

--- src/component/sheet/Cell.tsx
+++ src/component/sheet/Cell.tsx
@@ -1,14 +1,53 @@
+import { memo, useRef, useEffect } from "react";
 import styles from "./Cell.module.css";
 import { getColName } from "@/utils/getColName";
+import { useFocusStore } from "./store/useFocusStore";
+
 type CellProps = {
   row: number;
   col: number;
 };
 
-export default function Cell({ row, col }: CellProps) {
+function Cell({ row, col }: CellProps) {
+  const isFocused = useFocusStore(
+    (s) => s.focus?.row === row && s.focus?.col == col
+  );
+  const setFocus = useFocusStore((s) => s.setFocus);
+  const move = useFocusStore((s) => s.move);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isFocused) ref.current?.focus();
+  }, [isFocused]);
+
   return (
-    <div className={styles.container}>
+    <div
+      ref={ref}
+      tabIndex={0} // tabIndex => 이 요소가 키보드 포커스를 받을 수 있게 만든다
+      role="gridcell" // 시멘틱, 접근성을 위해, 브라우저에게 알려줌
+      className={`${styles.container} ${isFocused ? styles.focused : ""}`}
+      onMouseDown={() => setFocus({ row, col })}
+      onFocus={() => setFocus({ row, col })}
+      onKeyDown={(e) => {
+        if (e.key === "ArrowUp") {
+          e.preventDefault();
+          move("up");
+        } else if (e.key === "ArrowDown") {
+          e.preventDefault();
+          move("down");
+        } else if (e.key === "ArrowLeft") {
+          e.preventDefault();
+          move("left");
+        } else if (e.key === "ArrowRight") {
+          e.preventDefault();
+          move("right");
+        }
+      }}
+    >
       {row + 1},{getColName(col)}
     </div>
   );
 }
+
+export default memo(Cell);
