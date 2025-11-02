42d4e8a77bb7c413b774355cc7d236deba1401c2
MJ <alswns8495@naver.com>
2025-11-02 17:15:21 +0900

    🎨 ToolBar 기본 구조 초기화

--- src/components/header/ToolBar.tsx
+++ src/components/header/ToolBar.tsx
@@ -1,4 +1,13 @@
 import styles from "./ToolBar.module.css";
 export default function ToolBar() {
-  return <div className={styles.toolBarConatiner}>ToolBar</div>;
+  return (
+    <div className={styles.toolBarConatiner}>
+      <div>ctrl Z</div>
+      <div>ctrl Y</div>
+      <div>글자 크기</div>
+      <div>글자 색상</div>
+      <div>배경 색상</div>
+      <div>테두리</div>
+    </div>
+  );
 }
